1ef8928000f4d135305e9495417ff7e985ae4516
BrendaAlejandro <brenda.alejandro.becerra123@gmail.com>
2024-07-05 12:45:18 -0500

    nuevo

--- src/main/java/com/cibertec/repositorio/CategoriaRepository.java
+++ src/main/java/com/cibertec/repositorio/CategoriaRepository.java
@@ -6,14 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cibertec.modelos.Categoria;
 
-import jakarta.transaction.Transactional;
-
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-	@Transactional
+    
+    @Query("SELECT c FROM Categoria c WHERE c.id_categoria = :id")
+    public Categoria buscarPorId(Integer id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO CATEGORIA (nombre) VALUES (:nombre)", nativeQuery = true)
+    void agregar(String nombre);
+
+    @Transactional
     @Modifying
     @Query("UPDATE Categoria c SET c.nombre = :nombre WHERE c.id_categoria = :id")
     void actualizar(Integer id, String nombre);
@@ -22,16 +30,15 @@ public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
     @Modifying
     @Query("DELETE FROM Categoria c WHERE c.id_categoria = :id")
     void eliminar(Integer id);
-    
-    Categoria findByNombre(String nombre);
+
+    @Query("SELECT MAX(c.id_categoria) FROM Categoria c WHERE c.nombre = :nombre")
+    Integer nuevoId(String nombre);
+
+    @Query("SELECT c FROM Categoria c WHERE c.nombre LIKE %:nombre%")
+    public List<Categoria> buscarPorNombre(String nombre);
 
     @Query("SELECT c.nombre FROM Categoria c")
-    List<String> findAllNombres();
-    @Query("SELECT c FROM Categoria c WHERE c.nombre LIKE %:nombre%")
-    List<Categoria> buscarPorNombre(String nombre);
-
-    @Query("SELECT c FROM Categoria c WHERE c.id_categoria = :id")
-    Categoria buscarPorId(Integer id);
+    List<String> listarTodosLosNombres();
     
-
+    Categoria findByNombre(String nombre);
 }
